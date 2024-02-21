d93b40049ee52c3df624437a726bcc37a89c5f85
jillianevelynjones <126296171+jillianevelynjones@users.noreply.github.com>
2024-02-21 13:54:13 -0500

    Update 2 NewNPCGenerator.py
    Expanded Races

--- .idea/NewNPCGenerator.py
+++ .idea/NewNPCGenerator.py
@@ -4,7 +4,31 @@ from NewNPC import NewNPC
 class NewNPCGenerator:
     def __init__(self, num_npcs):
         self.num_npcs = num_npcs
-        self.races = ["human", "elf", "dwarf"]
+        self.races = ["human", "wood elf", "mountain dwarf", "lightfoot halfling",
+                      "forest gnome", "stout halfling", "rock gnome",
+                      "dragonborn", "hill dwarf", "high elf",
+                      "half elf", "aarakocra", "half orc",
+                      "infernal tiefling", "protector aasimar", "tabaxi",
+                      "firbolg", "triton", "kenku",
+                      "scourge aasimar", "fallen aasimar", "goliath",
+                      "water genasi", "air genasi", "earth genasi",
+                      "fire genasi", "feral tiefling", "eladrin elf",
+                      "ghostwise halfling", "deep gnome", "lizardfolk",
+                      "duergar dwarf", "drow", "kobold", "orc",
+                      "hobgoblin", "goblin", "bugbear", "yaun-ti pureblood"]
+        self.race_weights = [1500, 350, 350, 350,
+                             350, 300, 300,
+                             250, 250, 250,
+                             250, 200, 200,
+                             100, 100, 90,
+                             80, 75, 70,
+                             65, 60, 55,
+                             50, 50, 50,
+                             50, 40, 35,
+                             30, 30, 25,
+                             10, 10, 5,
+                             5, 3, 3,
+                             2, 1]
         self.genders = ["male", "female"]
 
     def generate_npcs(self):
