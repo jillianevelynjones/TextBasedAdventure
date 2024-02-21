5e7d7e6f5c1a189f27e8781d6447bfa241b65794
jillianevelynjones <126296171+jillianevelynjones@users.noreply.github.com>
2024-02-21 15:48:17 -0500

    Update 6 NPCGenerator.py
    Hair length weight based on race

--- Random NPC Generator/NPCGenerator.py
+++ Random NPC Generator/NPCGenerator.py
@@ -1,14 +1,9 @@
 import random
-from NPC import NPC
-from NPCNames import NPCNames
+from NewNPC import NewNPC
 
-
-class NPCGenerator:
+class NewNPCGenerator:
     def __init__(self, num_npcs):
-        self.npc_names = NPCNames(race, gender, age)
-        self.first_names = self.npc_names.generate_names()
         self.num_npcs = num_npcs
-        # self.npcs = []
         self.races = ["human", "wood elf", "mountain dwarf", "lightfoot halfling",
                       "forest gnome", "stout halfling", "rock gnome",
                       "dragonborn", "hill dwarf", "high elf",
@@ -52,58 +47,18 @@ class NPCGenerator:
         self.skin_color_weights = [self.skin_colors.count(color) for color in self.skin_colors]
         self.hair_lengths = ["long", "medium length", "short", "buzzed", "no", "error - hair length"]
         self.hair_length_weights = []
-        self.hair_colors = ["brown", "black", "blonde", "ginger", "red", "orange", "green", "blue", "purple"]
-        self.hair_color_weights = []
-        self.hair_textures = ["straight", "wavy", "curly", "kinky curly"]
-        self.hair_texture_weights = []
-        self.facial_hairs = [" ", "stubble", "a short beard", "a long beard", "a mustache", "mutton chops"]
-        self.facial_hair_weights = []
-        self.physical_features1 = ["completely normal", " ", "fat", "skinny",
-                                   "short", "tall", "built", "wearing glasses",
-                                   "missing", "scarred"]
-        self.physical_features2 = [" ", "butterfly tattoo", "rose tattoo",
-                                   "dragon tattoo", "flower tattoo", "snake tattoo",
-                                   "geometric tattoo", "lion tattoo", "heart tattoo",
-                                   "skull tattoo", "tribal tattoo", "nautical tattoo",
-                                   "moon tattoo", "pierced ear", "pierced cartilage",
-                                   "pierced nose", "eyebrow piercing", "lip piercing",
-                                   "an ear", "an arm", "a hand",
-                                   "a finger", "both arms", "a leg",
-                                   "both legs", "above the eye", "across the nose",
-                                   "on the cheek", "on the shoulder", "on their arm",
-                                   "on their hand", "on their back", "on their leg",
-                                   "error - physical feature 2"]
-        self.speech_patterns1 = ["in a normal tone", "in a soft spoken voice", "loudly",
-                                 "quickly", "slowly", "with a heavy",
-                                 "in a mumble", "in a stutter", "with foul language"]
-        self.speech_patterns2 = ["midwestern accent", "english accent", "scottish accent",
-                                 "russian accent", "french accent", "southern accent",
-                                 "californian accent"]
-        self.personality_traits = ["arrogant", "brave", "cowardly", "deceitful", "honest", "loyal"]
-        self.mannerisms = ["nervous", "confident", "cocky", "polite", "rude"]
 
     def generate_npcs(self):
         npcs = []
         for i in range(self.num_npcs):
-            race = random.choices(self.races, weights=self.race_weights)[0]
+            race = random.choice(self.races)
             mean_age = 30
             stdev_age = 10
             age = int(random.normalvariate(mean_age, stdev_age))
-            gender = random.choices(self.genders, weights=self.gender_weights)[0]
-            npc_names.append(self.get_random_name())
-            first_name = npc_names.generate_names()
-            skin_color = random.choices(self.skin_colors, weights=self.skin_color_weights)[0]
-            physical_feature1 = random.choice(self.physical_features1)
-            physical_feature2 = random.choice(self.physical_features2)
-            hair_color = random.choices(self.hair_colors, weights=self.hair_color_weights)[0]
-            hair_length = random.choices(self.hair_lengths, weights=self.hair_length_weights)[0]
-            hair_texture = random.choices(self.hair_textures, weights=self.hair_texture_weights)[0]
-            facial_hair = random.choices(self.facial_hairs, weights=self.facial_hair_weights)[0]
-            speech_patterns1 = random.choice(self.speech_patterns1)
-            speech_patterns2 = random.choice(self.speech_patterns2)
-            personality_traits = random.choice(self.personality_traits)
-            mannerisms = random.choice(self.mannerisms)
-
+            gender = random.choice(self.genders)
+            skin_color = random.choice(self.skin_colors)
+            hair_length = random.choice(self.hair_lengths)
+            
             if race in ["human", "high elf", "wood elf", "eladrin elf", "hill dwarf", "mountain dwarf",
                         "stout halfling",
                         "lightfoot halfling", "ghostwise halfling", "forest gnome", "rock gnome", "deep gnome",
@@ -152,80 +107,20 @@ class NPCGenerator:
                 self.skin_color = ["brown fur"]
             else:
                 self.skin_color = ["error - skin color races"]
-
             if race in ["human", "high elf", "wood elf", "eladrin elf", "hill dwarf", "mountain dwarf",
                         "stout halfling", "lightfoot halfling", "ghostwise halfling", "forest gnome", "rock gnome",
                         "deep gnome", "half elf", "protector aasimar", "scourge aasimar", "fallen aasimar", "half orc",
                         "goliath", "air genasi", "earth genasi", "fire genasi", "water genasi", "duergar dwarf", "drow",
                         "hobgoblin", "goblin", "yuan-ti pureblood"]:
-                if gender in ["female"]:
-                    self.hair_length = ["long", "medium length", "short", "buzzed"]
-                    self.hair_lengths_weights = [4, 3, 2, 1]
-                elif gender in ["male"]:
-                    self.hair_length = ["long", "medium length", "short", "no"]
-                    self.hair_lengths_weights = [2, 3, 4, 1]
-                elif gender in ["non binary"]:
-                    self.hair_length = ["long", "medium length", "short", "no"]
-                else:
-                    self.hair_length = ["error - hair length"]
+                self.hair_length = ["long", "medium length", "short", "buzzed", "no"]
+                self.hair_length_weights = [4, 3, 2, 1, 1]
             else:
-                self.hair_length = [" "]
-
-            if race in ["human", "high elf", "wood elf",
-                        "eladrin elf", "hill dwarf", "mountain dwarf",
-                        "stout halfling", "lightfoot halfling", "ghostwise halfling",
-                        "forest gnome", "rock gnome", "deep gnome",
-                        "half elf", "protector aasimar", "scourge aasimar",
-                        "fallen aasimar"]:
-                if hair_length != "no":
-                    self.hair_texture = [" "]
-                else:
-                    self.hair_texture = ["straight", "wavy", "curly", "kinky curly"]
-
-            if gender in ["male"]:
-                self.facial_hair = ["a clean shaven face", "stubble", "a short beard", "a long beard", "a mustache",
-                                    "mutton chops"]
-                self.facial_hair_weights = [20, 10, 5, 5, 1, 1]
-            elif gender in ["non binary"]:
-                self.facial_hair = [" ", "stubble", "a short beard", "a long beard", "a mustache", "mutton chops"]
-                self.facial_hair_weights = [50, 1, 1, 1, 1, 1]
-            else:
-                self.facial_hair = [" "]
-
-            if physical_feature1 in ["completely normal", "fat",
-                                     "skinny", "built", "wears glasses"]:
-                self.physical_feature2 = " "
-            elif physical_feature2 in [" "]:
-                self.physical_feature2 = ["butterfly tattoo", "rose tattoo",
-                                          "dragon tattoo", "flower tattoo", "snake tattoo",
-                                          "geometric tattoo", "lion tattoo", "heart tattoo",
-                                          "skull tattoo", "tribal tattoo", "nautical tattoo",
-                                          "moon tattoo", "pierced ear", "pierced cartilage",
-                                          "pierced nose", "eyebrow piercing", "lip piercing"]
-            elif physical_feature1 in ["missing"]:
-                self.physical_feature2 = ["an ear", "an arm", "a hand",
-                                          "a finger", "both arms", "a leg",
-                                          "both legs"]
-            elif physical_feature1 in ["scarred"]:
-                self.physical_feature2 = ["above the eye", "across the nose",
-                                          "on the cheek", "on the shoulder", "on their arm",
-                                          "on their hand", "on their back", "on their leg"]
-            else:
-                self.physical_feature2 = ["error - physical feature 2"]
-
-            if speech_patterns1 in ["with a heavy"]:
-                self.speech_pattern2 = ["midwestern accent", "english accent", "scottish accent",
-                                        "russian accent", "french accent", "southern accent",
-                                        "californian accent"]
-            else:
-                self.speech_pattern2 = [" "]
-
-            npc = NPC(first_name, race, age, gender, skin_color,
-                      physical_feature1, physical_feature2, hair_color,
-                      hair_length, hair_texture, facial_hair,
-                      speech_patterns1, speech_pattern2, personality_traits,
-                      mannerisms)
+                self.hair_length = ["error - hair length"]
+                self.hair_length_weights = [1]
+            
+            npc = NewNPC(race, age, gender, skin_color, hair_length)
             npcs.append(npc)
-
         self.npcs = npcs
+
         return npcs
+    
