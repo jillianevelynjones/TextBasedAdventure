d39719a6873899c7eb14dfd7b3eba771c28fd904
jillianevelynjones <126296171+jillianevelynjones@users.noreply.github.com>
2024-02-21 14:07:51 -0500

    Update 4 NewNPC.py
    Add Skin Color

--- .idea/NewNPC.py
+++ .idea/NewNPC.py
@@ -1,8 +1,9 @@
 class NewNPC:
-    def __init__(self, race, age, gender):
+    def __init__(self, race, age, gender, skin_color):
         self.race = race
         self.age = age
         self.gender = gender
+        self.skin_color = skin_color
 
     def __str__(self):
-        return f"NPC is a {self.age} year old {self.gender} {self.race}"
+        return f"NPC is a {self.age} year old {self.gender} {self.race} with {self.skin_color}"
