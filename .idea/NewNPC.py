d3c3830cc402c9248e3bb8f9ec29b830c01db7d0
jillianevelynjones <126296171+jillianevelynjones@users.noreply.github.com>
2024-02-21 15:42:54 -0500

    Update 5 NewNPC.py
    Add unweighted hair length

--- .idea/NewNPC.py
+++ .idea/NewNPC.py
@@ -1,9 +1,10 @@
 class NewNPC:
-    def __init__(self, race, age, gender, skin_color):
+    def __init__(self, race, age, gender, skin_color, hair_length):
         self.race = race
         self.age = age
         self.gender = gender
         self.skin_color = skin_color
+        self.hair_length = hair_length
 
     def __str__(self):
-        return f"NPC is a {self.age} year old {self.gender} {self.race} with {self.skin_color}"
+        return f"NPC is a {self.age} year old {self.gender} {self.race} with {self.skin_color} and {self.hair_length}"
