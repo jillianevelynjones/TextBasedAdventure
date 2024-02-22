976927a30bcc3565de2bd91f67297477b427e50a
jillianevelynjones <126296171+jillianevelynjones@users.noreply.github.com>
2024-02-22 12:25:06 -0500

    Update 12 NewNPC.py
    added physical feature 1 and did some spacing

--- .idea/NewNPC.py
+++ .idea/NewNPC.py
@@ -1,5 +1,5 @@
 class NewNPC:
-    def __init__(self, race, age, gender, skin_color, hair_length, hair_texture, hair_color, facial_hair):
+    def __init__(self, race, age, gender, skin_color, hair_length, hair_texture, hair_color, facial_hair, physical_feature1):
         self.race = race
         self.age = age
         self.gender = gender
@@ -8,6 +8,7 @@ class NewNPC:
         self.hair_texture = hair_texture
         self.hair_color = hair_color
         self.facial_hair = facial_hair
+        self.physical_feature1 = physical_feature1
 
     def __str__(self):
-        return f"NPC is age: {self.age} gender: {self.gender} race: {self.race} skin color: {self.skin_color} hair: {self.hair_length} {self.hair_texture} {self.hair_color} facial hair: {self.facial_hair}"
+        return f"NPC is age: {self.age} \n    gender: {self.gender} \n    race: {self.race} \n    skin color: {self.skin_color} \n    hair: {self.hair_length} {self.hair_texture} {self.hair_color} \n    facial hair: {self.facial_hair} \n    physical feature: {self.physical_feature1}"
