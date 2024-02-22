97fdd28a8f773a24746643f8fc2bb9d707e45b38
jillianevelynjones <126296171+jillianevelynjones@users.noreply.github.com>
2024-02-22 14:57:15 -0500

    Update 16 NewNPC.py
    Adding Names

--- .idea/NewNPC.py
+++ .idea/NewNPC.py
@@ -1,5 +1,8 @@
+from NewNPCName import NewNPCName
+
 class NewNPC:
     def __init__(self, race, age, gender, skin_color, hair_length, hair_texture, hair_color, facial_hair, physical_feature1, physical_feature2, speech_pattern1, speech_pattern2, personality_trait, mannerism):
+        self.name = NewNPCName(race, gender).generate_names()
         self.race = race
         self.age = age
         self.gender = gender
@@ -16,4 +19,4 @@ class NewNPC:
         self.mannerism = mannerism
 
     def __str__(self):
-        return f"NPC is age: {self.age} \n    gender: {self.gender} \n    race: {self.race} \n    skin color: {self.skin_color} \n    hair: {self.hair_length} {self.hair_texture} {self.hair_color} \n    facial hair: {self.facial_hair} \n    physical feature: {self.physical_feature1} {self.physical_feature2} \n    speech: {self.speech_pattern1} {self.speech_pattern2} \n    personality trait: {self.personality_trait} \n    mannerisms: {self.mannerism}"
+        return f"NPC: {self.name} \n     age: {self.age} \n    gender: {self.gender} \n    race: {self.race} \n    skin color: {self.skin_color} \n    hair: {self.hair_length} {self.hair_texture} {self.hair_color} \n    facial hair: {self.facial_hair} \n    physical feature: {self.physical_feature1} {self.physical_feature2} \n    speech: {self.speech_pattern1} {self.speech_pattern2} \n    personality trait: {self.personality_trait} \n    mannerisms: {self.mannerism}"
