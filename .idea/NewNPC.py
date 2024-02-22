6f94b517ec38d7dd839f44a068db2c4e7730f8d0
jillianevelynjones <126296171+jillianevelynjones@users.noreply.github.com>
2024-02-22 12:53:30 -0500

    Update 13 NewNPC.py
    added physical feature 2

--- .idea/NewNPC.py
+++ .idea/NewNPC.py
@@ -1,5 +1,5 @@
 class NewNPC:
-    def __init__(self, race, age, gender, skin_color, hair_length, hair_texture, hair_color, facial_hair, physical_feature1):
+    def __init__(self, race, age, gender, skin_color, hair_length, hair_texture, hair_color, facial_hair, physical_feature1, physical_feature2):
         self.race = race
         self.age = age
         self.gender = gender
@@ -9,6 +9,7 @@ class NewNPC:
         self.hair_color = hair_color
         self.facial_hair = facial_hair
         self.physical_feature1 = physical_feature1
+        self.physical_feature2 = physical_feature2
 
     def __str__(self):
-        return f"NPC is age: {self.age} \n    gender: {self.gender} \n    race: {self.race} \n    skin color: {self.skin_color} \n    hair: {self.hair_length} {self.hair_texture} {self.hair_color} \n    facial hair: {self.facial_hair} \n    physical feature: {self.physical_feature1}"
+        return f"NPC is age: {self.age} \n    gender: {self.gender} \n    race: {self.race} \n    skin color: {self.skin_color} \n    hair: {self.hair_length} {self.hair_texture} {self.hair_color} \n    facial hair: {self.facial_hair} \n    physical feature: {self.physical_feature1} {self.physical_feature2}"
