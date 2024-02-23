c7a6d155d5bfcc6b602d028765ad1725750d8642
Jillian <jillianevelynjones@gmail.com>
2024-02-22 21:56:51 -0500

    It's a start!... again

--- TextBasedAdventure/character_generator.py
+++ TextBasedAdventure/character_generator.py
@@ -2,37 +2,14 @@ import random
 
 
 def character_generator():
-    print("..........")
-    print("First, choose a race.")
-    print("Options are: ")
-    print("Dwarf, ")
-    print("Elf, ")
-    print("Halfling, ")
-    print("Human, ")
-    print("Dragonborn, ")
-    print("Gnome, ")
-    print("Half-Elf, ")
-    print("Half-Orc, ")
-    print("and Tiefling.")
-    raceDesc = input("To learn more about a race, please type their name, if not type 'choose' :\n")
-    if raceDesc == "choose":
-        raceChoice = input("Please choose your race: ")
-    if raceDesc in ["Dwarf", "dwarf"]:
-        print("..........")
-        print("Bold and hardy, dwarves are known as skilled warriors, miners, and workers of stone and metal. Though they stand well under 5 feet tall, dwarves are so broad and compact that they can weigh as much as a human standing nearly two feet taller. Their courage and endurance are also easily a match for any of the larger folk.")
-        print("Your dwarf character has an assortment of inborn abilities, part and parcel of dwarven nature")
-        print("Ability Score Increase. Your Constitution score increases by 2.")
-        print("Age. Dwarves mature at the same rate as humans, but they’re considered young until they reach the age of 50. On average, they live about 350 years.")
-        print("Size. Dwarves stand between 4 and 5 feet tall and average about 150 pounds. Your size is Medium")
-        print("Speed. Your base walking speed is 25 feet. Your speed is not reduced by wearing heavy armor.")
-        print("Darkvision. Accustomed to life underground, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can’t discern color in darkness, only shades of gray.")
-        print("Dwarven Resilience. You have advantage on saving throws against poison, and you have resistance against poison damage")
-        print("Dwarven Combat Training. You have proficiency with the battleaxe, handaxe, light hammer, and warhammer.")
-        print("Tool Proficiency. You gain proficiency with the artisan’s tools of your choice: smith’s tools, brewer’s supplies, or mason’s tools.")
-        print("Stonecunning. Whenever you make an Intelligence (History) check related to the origin of stonework, you are considered proficient in the History skill and add double your proficiency bonus to the check, instead of your normal proficiency bonus")
-        print("Languages. You can speak, read, and write Common and Dwarvish. Dwarvish is full of hard consonants and guttural sounds, and those characteristics spill over into whatever other language a dwarf might speak.")
-        print("Subrace. Two main subraces of dwarves populate the worlds of D&D: hill dwarves and mountain dwarves. Choose one of these subraces.")
-    else:
-        print("..........")
-        print("The End")
+    print('  \n   There are two possible characters: 1 & 2')
+    character_choice = input ('\n > ')
 
+    if character_choice == '1':
+        print("  \n   you chose Character 1")
+
+    elif character_choice == '2':
+        print("  \n   you chose Character 2")
+
+    else:
+        print("  \n   Oops!")
--- TextBasedAdventure/main.py
+++ TextBasedAdventure/main.py
@@ -1,12 +1,106 @@
 from character_generator import character_generator
+from os import system
+import time
+import datetime
+import pickle
+import sys
 
 
-def main():
-    print("Welcome to The Saga of the Infected: Secrets and Prophecies of Krysmanthia")
-    print("The Saga of the Infected is based on Dungeons and Dragons 5th Edition. For game rules please refer to the Players Handbook")
-    print("First we must make your character!")
+#miscellaneous functions + procedures
+
+def w(t):
+    time.sleep(t)
+
+def version_counter(filename="adventure_colussus_version_counter.dat"):
+    with open(filename, "a+") as f:
+        f.seek(0)
+        val = int(f.read() or 0) + 1
+        f.seek(0)
+        f.truncate()
+        f.write(str(val))
+        return val
+counter = version_counter()
+
+def t(text):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.021)
+
+def save_character(save_name, character):
+    save_name_pickle = save_name + '.pickle'
+    t('> saving character')
+    w(1)
+    with open(save_name_pickle, 'wb') as f:
+        pickle.dump(character, f)
+        t('> character saved successfully')
+
+def load_character(load_name):
+    load_name_pickle = load_name + '.pickle'
+    t(' > loading character...')
+    w(1)
+    pickle_in = open(load_name_pickle,"rb")
+    character = pickle.load(pickle_in)
+    t(' > character loaded successfully\n')
+    t('\n> welcome back ')
+    t(character['name'])
+    t('!!!\n')
+    w(0.5)
+    t('\n> here are your stats from last time: \n')
+    print(' >',character)
+
+
+# main game
+
+w(0.5)
+e = datetime.datetime.now()
+print('\n  <Saga of the Infected>         version: v', counter, '| current date: ', e)
+w(0.5)
+print('\n > [1] create new game')
+print(' > [2] load existing game')
+print(' > [3] end game')
+print(' > [4] credits')
+choice = input("\n > ")
+
+if choice == '1':
+
+    t("\n > you have chosen to create a new game: redirecting...")
+    w(0.75)
+    system('cls')
+    print('  \n   we will begin with creating your character: ')
+    w(0.5)
+    w(0.3)
+
     character_generator()
 
 
-if __name__ == "__main__":
-    main()
+
+
+elif choice == '2':
+
+    t("\n > you have chosen to load an existing game: redirecting...")
+    w(0.75)
+    system('cls')
+    w(0.5)
+    print(
+        '  \n  we will begin with choosing an existing character:                             quick tip: make sure it exists!')
+    w(0.5)
+
+    character_file_name = input('\n > character file name: ')
+    load_character(character_file_name)
+
+
+
+elif choice == '3':
+
+    t(' > ending session...')
+    w(0.5)
+    t(' > session ended successfully')
+    w(1)
+    sys.exit()
+
+elif choice == '4':
+    pass
+
+else:
+    t('incorrect response. please try again')
