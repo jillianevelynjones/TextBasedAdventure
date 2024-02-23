138abde0467f05c540e2231903f506ab5957d9bc
Jillian <jillianevelynjones@gmail.com>
2024-02-22 20:56:04 -0500

    It's a start!

---
+++ TextBasedAdventure/main.py
@@ -0,0 +1,12 @@
+from character_generator import character_generator
+
+
+def main():
+    print("Welcome to The Saga of the Infected: Secrets and Prophecies of Krysmanthia")
+    print("The Saga of the Infected is based on Dungeons and Dragons 5th Edition. For game rules please refer to the Players Handbook")
+    print("First we must make your character!")
+    character_generator()
+
+
+if __name__ == "__main__":
+    main()
