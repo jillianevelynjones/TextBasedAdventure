f68473b1d620dd83ce74f8b154858324eb1620fc
Jillian <jillianevelynjones@gmail.com>
2024-02-23 14:22:45 -0500

    Figured out how to make scores in a loop in case of invalid answer

--- TextBasedAdventure/character_generator.py
+++ TextBasedAdventure/character_generator.py
@@ -1,4 +1,33 @@
 def character_generator():
+    '''CharName = "Character 1"
+    Race = input("race: ")
+    charClass = input("class: ")'''
+
+    print("   \n   ......................")
+    print("  \n   We'll start with choosing Ability Scores.")
+    print("  Ability score options are 15, 14, 13, 12, 10, 8.")
+    print("  You must use one of each.")
+    print("  Ability options are Strength, Dexterity, Constitution, Intelligence, Wisdom, Charisma")
+    while True:
+        STRscore = input("\n > Enter Strength score")
+        print("  \n   Strength score now ", STRscore)
+        if STRscore == "15":
+            print("   Remaining score options: 14, 13, 12, 10, 8")
+            while True:
+                DEXscore = input("  \n   Enter Dexterity score")
+                print("  \n   Strength Score now ", STRscore)
+                print("   Dexterity Score now ", DEXscore)
+                if DEXscore == "14":
+                    print("  \n   Remaining score options 13, 12, 10, 8")
+                    break
+                else:
+                    print("  \n   Dexterity NOT 14")
+                    continue
+        else:
+            print("  \n   Strength NOT 15")
+            continue
+        break
+    '''
     print('  \n   There are two possible characters: 1 & 2')
     character_choice = input ('\n > ')
 
@@ -12,3 +41,4 @@ def character_generator():
         print("  \n   Oops!")
 
     return character_choice
+    '''
--- TextBasedAdventure/main.py
+++ TextBasedAdventure/main.py
@@ -52,59 +52,61 @@ def load_character(load_name):
 def intro():
     w(0.5)
     e = datetime.datetime.now()
-    print('\n  <Saga of the Infected>         version: v', counter, '| current date: ', e)
-    w(0.5)
-    print('\n > [1] create new game')
-    print(' > [2] load existing game')
-    print(' > [3] end game')
-    print(' > [4] credits')
-    choice = input("\n > ")
-
-    if choice == '1':
-
-        t("\n > you have chosen to create a new game: redirecting...")
-        w(0.75)
-        system('cls')
-        print('  \n   we will begin with creating your character: ')
+    while True:
+        print('\n  <Saga of the Infected>         version: v', counter, '| current date: ', e)
         w(0.5)
-        w(0.3)
+        print('\n > [1] create new game')
+        print(' > [2] load existing game')
+        print(' > [3] end game')
+        print(' > [4] credits')
+        choice = input("\n > ")
 
-        character = character_generator()
-        print("  \n   pick a file name to save under")
-        character_file_name = input('> ')
-        save_character(character_file_name, character)
-        print("  \n   game saved as: ", character_file_name)
+        if choice == '1':
+            t("\n > you have chosen to create a new game: redirecting...")
+            w(0.75)
+            system('cls')
+            print("   \n   ......................")
+            print('  \n   we will begin with creating your character: ')
+            w(0.5)
+            w(0.3)
+
+            character = character_generator()
+            print("  \n   THE END")
+            '''print("  \n   pick a file name to save under")
+            character_file_name = input('> ')
+            save_character(character_file_name, character)
+            print("  \n   game saved as: ", character_file_name)'''
+            break
+
+        elif choice == '2':
+            t("\n > you have chosen to load an existing game: redirecting...")
+            w(0.75)
+            system('cls')
+            w(0.5)
+            print('  \n  we will begin with choosing an existing character:')
+            w(0.5)
+
+            character_file_name = input('\n > character file name: ')
+            load_character(character_file_name)
+            break
 
 
 
+        elif choice == '3':
+            t(' > ending session...')
+            w(0.5)
+            t(' > session ended successfully')
+            w(1)
+            sys.exit()
+            break
 
-    elif choice == '2':
+        elif choice == '4':
+            pass
+            break
 
-        t("\n > you have chosen to load an existing game: redirecting...")
-        w(0.75)
-        system('cls')
-        w(0.5)
-        print('  \n  we will begin with choosing an existing character:')
-        w(0.5)
-
-        character_file_name = input('\n > character file name: ')
-        load_character(character_file_name)
-
-
-
-    elif choice == '3':
-
-        t(' > ending session...')
-        w(0.5)
-        t(' > session ended successfully')
-        w(1)
-        sys.exit()
-
-    elif choice == '4':
-        pass
-
-    else:
-        t('incorrect response. please try again')
+        else:
+            t('incorrect response. please try again')
+            continue
 
 if __name__ == "__main__":
     intro()
