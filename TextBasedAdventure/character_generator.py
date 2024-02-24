bbc5573df9b5fa6fdbe22684cde231f50ffc70bf
Jillian <jillianevelynjones@gmail.com>
2024-02-23 20:53:44 -0500

    Added race: human

--- TextBasedAdventure/character_generator.py
+++ TextBasedAdventure/character_generator.py
@@ -1,14 +1,36 @@
 def character_generator():
     ability_scores = {
-        "Strength": None,
-        "Dexterity": None,
-        "Constitution": None,
-        "Intelligence": None,
-        "Wisdom": None,
-        "Charisma": None
+        "Strength": 0,
+        "Dexterity": 0,
+        "Constitution": 0,
+        "Intelligence": 0,
+        "Wisdom": 0,
+        "Charisma": 0
     }
 
     score_options = ["15", "14", "13", "12", "10", "8"]
+
+    print("  \n   ......................")
+    print("  \n   First choose a race.")
+    print("   Options are: ")
+    print("      Human")
+    race = input(f"   Enter race: ").lower()
+    if race in ("human", "Human"):
+        print("  \n   As a human all your ability scores increase by 1")
+        print("   Your size is medium")
+        size = 5
+        print("   Your speed is 30")
+        speed = 30
+        print("   You can speak, read and write in Common and one extra language of your choice")
+        language1 = ("common")
+        print("  \n   Your choices are: ")
+        print("      Elvish")
+        language2 = input(f"   Enter language: ")
+        if language2 in ("elvish", "Elvish"):
+            language2 = ("elvish")
+        else:
+            print("Sorry! Only language available at the time is Elvish")
+            language2 = ("evlish")
 
     print("   \n   ......................")
     print("  \n  Now we'll choose Ability Scores.")
@@ -29,6 +51,55 @@ def character_generator():
             else:
                 print(f"  \n   Invalid {ability} score. Please choose from {', '.join(score_options)}.")
 
+    if race in ("human", "Human"):
+        for ability in ability_scores:
+            ability_scores[ability] = str(int(ability_scores[ability]) + 1)
+    else:
+        print("Error add +1 to ability scores for being human")
+
     print("\nFinal Ability Scores:")
     for ability, score in ability_scores.items():
         print(f"{ability}: {score}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def print_character(size, race, level, charClass, speed, ability_scores, language1, language2, prof):
+    #Name
+
+    if size == "5":
+        print("Medium ", race)
+    else:
+        print("Error", race)
+
+    print("Level ", level, charClass)
+
+    print("  \n   ......................")
+
+    #AC
+    #HP
+    print("Speed ", speed, " ft.")
+
+    print("  \n   ......................")
+
+    print("\nAbility Scores:")
+    for ability, score in ability_scores.items():
+        print(f"{ability}: {score}")
+
+    print("  \n   ......................")
+
+    print("Languages", language1, language2)
+
+    print("Proficiency Bonus +", prof)
+
