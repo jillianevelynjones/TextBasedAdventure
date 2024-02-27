68763a726f193537b1b656957f51092c761d892e
jillianevelynjones <126296171+jillianevelynjones@users.noreply.github.com>
2024-02-26 22:06:08 -0500

    Update char_class_gen.py
    Fixed skills proficiency
    Now have issue with hit points

--- TextBasedAdventure/char_class_gen.py
+++ TextBasedAdventure/char_class_gen.py
@@ -1,86 +1,126 @@
+def initialize_skills_prof():
+	skills_proficiency_dict = {
+	  "light armor": False,
+	  "medium armor": False,
+	  "heavy armor": False,
+	  "shields": False,
+	  "simple weapons": False,
+	  "martial weapons": False,
+	  "strength save": False,
+	  "dexterity save": False,
+	  "constitution save": False,
+	  "intelligence save": False,
+	  "wisdom save": False,
+	  "charisma save": False,
+	  "athletics": False,
+	  "acrobatics": False,
+	  "sleight of hand": False,
+	  "stealth": False,
+	  "arcana": False,
+	  "history": False,
+	  "investigation": False,
+	  "nature": False,
+	  "religion": False,
+	  "animal handling": False,
+	  "insight": False,
+	  "medicine": False,
+	  "perception": False,
+	  "survival": False,
+	  "deception": False,
+	  "intimidation": False,
+	  "performance": False,
+	  "persuasion": False,
+	  "": False
+	  }
+	return skills_proficiency_dict
 
 def char_class_gen(skills_proficiency_dict):
-  print("  \n   ......................")
-  print("  \n   Now it's time to choose your class")
-  print("   Options are: ")
-  print("      Fighter")
-  while True:
-    char_class = input(f"   Enter class: ")
-    if char_class in ("fighter", "Fighter"):
-      class1 = "Fighter"
-      char_class = fighter(skills_proficiency_dict)
-      break
-    else:
-      print("Sorry! Only fighter is available")
-      continue
-  return char_class
+    print("\n   ......................")
+    print("\n   Now it's time to choose your class")
+    print("   Options are: ")
+    print("      Fighter")
+    while True:
+        char_class = input(f"   Enter class: ")
+        if char_class in ("fighter", "Fighter"):
+            class1 = "Fighter"
+            char_class = fighter(skills_proficiency_dict)
+            break
+        else:
+            print("Sorry! Only fighter is available")
+            continue
+    return char_class
 
 def fighter(skills_proficiency_dict):
-  print("  \n   As a fighter your hit dice are 1d10")
-  print("  \n   ......................")
+    print("\n   As a fighter your hit dice are 1d10")
+    print("\n   ......................")
+    
+    print("\n   You are also now proficienct in all armor, shields and weapons.")
+    print("   You can now add your proficiency bonus to Strength and Constitution Saving Throws.")
+    
+    skills_proficiency_dict["light armor"] = True
+    skills_proficiency_dict["medium armor"] = True
+    skills_proficiency_dict["heavy armor"] = True
+    skills_proficiency_dict["shields"] = True
+    skills_proficiency_dict["simple weapons"] = True
+    skills_proficiency_dict["martial weapons"] = True
+    skills_proficiency_dict["strength save"] = True
+    skills_proficiency_dict["constitution save"] = True
+    
+    print("\n   ......................")
+    
+    print("\n   You're also proficient in your choice of two of the following: ")
+    print("      1. Acrobatics")
+    print("      2. Animal Handling")
+    print("      3. Athletics")
+    print("      4. History")
+    print("      5. Insight")
+    print("      6. Intimidation")
+    print("      7. Perception")
 
-  skills_proficiency_dict["light armor"] = True
-  skills_proficiency_dict["medium armor"] = True
-  skills_proficiency_dict["heavy armor"] = True
-  skills_proficiency_dict["shields"] = True
-  skills_proficiency_dict["simple weapons"] = True
-  skills_proficiency_dict["martial weapons"] = True
-  skills_proficiency_dict["strength save"] = True
-  skills_proficiency_dict["constitution save"] = True
+    chosen_skills = []
+    temp_skill = ""
+    while len(chosen_skills) < 2:
+        choice = input("Skill: > ")
+        if choice.isdigit():
+            choice = int(choice)
+            if 1 <= choice <= 7:
+                if choice == 1:
+                    temp_skill = "acrobatics"
+                elif choice == 2:
+                    temp_skill = "animal handling"
+                elif choice == 3:
+                    temp_skill = "athletics"
+                elif choice == 4:
+                    temp_skill = "history"
+                elif choice == 5:
+                    temp_skill = "insight"
+                elif choice == 6:
+                    temp_skill = "intimidation"
+                elif choice == 7:
+                    temp_skill = "perception"
 
-  print("  \n   You're also proficient in your choice of two of the following: ")
-  print("      1. Acrobatics")
-  print("      2. Animal Handling")
-  print("      3. Athletics")
-  print("      4. History")
-  print("      5. Insight")
-  print("      6. Intimidation")
-  print("      7. Perception")
-
-  chosen_skills = []
-  temp_skill = ""
-  while len(chosen_skills) < 2:
-    choice = input("Skill: > ")
-    if choice.isdigit():
-      choice = int(choice)
-      if 1 <= choice <= 7:
-        if choice == 1:
-          temp_skill = "acrobatics"
-        elif choice == 2:
-          temp_skill = "animal handling"
-        elif choice == 3:
-          temp_skill = "athletics"
-        elif choice == 4:
-          temp_skill = "history"
-        elif choice == 5:
-          temp_skill = "insight"
-        elif choice == 6:
-          temp_skill = "intimidation"
-        elif choice == 7:
-          temp_skill = "perception"
-
-        if not skills_proficiency_dict[temp_skill]:
-          skills_proficiency_dict[temp_skill] = True
-          chosen_skills.append[temp_skill]
-          print(f"You are now proficient in {temp_skill}.")
+                if not skills_proficiency_dict[temp_skill]:
+                    skills_proficiency_dict[temp_skill] = True
+                    chosen_skills.append(temp_skill)
+                    print(f"You are now proficient in {temp_skill}.")
+                else:
+                    print("You have already chosen proficiency in that skill.")
+            else:
+                print("Invalid choice. Please enter a number between 1 and 7.")
         else:
-          print("You have already chosen proficiency in that skill.")
-      else:
-        print("Invalid choice. Please enter a number between 1 and 7.")
-    else:
-      print("Invalid input. Please enter a number.")
+            print("Invalid input. Please enter a number.")
 
-  print("  \n   ......................")
-  print("  \n   Now it's time to choose your fighting style")
-  print("   Options are: ")
-  print("      Defense")
-  while True:
-    char_class = input(f"   Enter fighting style: ")
-    if char_class in ("defense", "Defense"):
-      # AC SHIT
-      break
-    else:
-      print("Sorry! Only defense is available")
-      continue
+    print("\n   ......................")
+    print("\n   Now it's time to choose your fighting style")
+    print("   Options are: ")
+    print("      Defense")
+    while True:
+        char_class = input(f"   Enter fighting style: ")
+        if char_class in ("defense", "Defense"):
+            # AC SHIT
+            break
+        else:
+            print("Sorry! Only defense is available")
+            continue
 
-  return "Fighter"
+    return "Fighter"
