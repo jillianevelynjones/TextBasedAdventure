c93d17bd8ce412367adeed151f57b75d98d71b44
jillianevelynjones <126296171+jillianevelynjones@users.noreply.github.com>
2024-02-28 10:13:55 -0500

    Initialization Module Added

--- TextBasedAdventure/TestCharacterGen.py
+++ TextBasedAdventure/TestCharacterGen.py
@@ -3,79 +3,6 @@ from CharClassGen import char_class_gen
 from CharRaceGen import char_race_gen
 
 score_options = ["15", "14", "13", "12", "10", "8"]
-
-def calculate_ability_bonus(score):
-    if score == "20":
-        return 5
-    elif score in ["18", "19"]:
-        return 4
-    elif score in ["16", "17"]:
-        return 3
-    elif score in ["14", "15"]:
-        return 2
-    elif score in ["12", "13"]:
-        return 1
-    elif score in ["10", "11"]:
-        return 0
-    elif score in ["8", "9"]:
-        return -1
-    elif score in ["6", "7"]:
-        return -2
-    elif score in ["4", "5"]:
-        return -3
-    elif score in ["2", "3"]:
-        return -4
-    elif score == "1":
-        return -5
-    else:
-        return None
-
-
-def save_character(save_name, attributes):
-    save_name_pickle = save_name + '.pickle'
-    print('> saving character')
-    try:
-        with open(save_name_pickle, 'w') as f:
-            f.write(attributes)
-        print('> character saved successfully')
-    except Exception as e:
-        print('> error saving character:', e)
-
-
-def assign_skills_to_ability(char_skills_proficiency_dict):
-    skill_ability_map = {
-      "athletics": "Strength",
-      "acrobatics": "Dexterity",
-      "sleight of hand": "Dexterity",
-      "stealth": "Dexterity",
-      "arcana": "Intelligence",
-      "history": "Intelligence",
-      "investigation": "Intelligence",
-      "nature": "Intelligence",
-      "religion": "Intelligence",
-      "animal handling": "Wisdom",
-      "insight": "Wisdom",
-      "medicine": "Wisdom",
-      "perception": "Wisdom",
-      "survival": "Wisdom",
-      "deception": "Charisma",
-      "intimidation": "Charisma",
-      "performance": "Charisma",
-      "persuasion": "Charisma"
-    }
-    return {skill: skill_ability_map[skill] for skill in char_skills_proficiency_dict}
-
-
-def print_skill_bonuses(skill_bonuses):
-    for skill, bonus in skill_bonuses.items():
-        if bonus > 0:
-            print("  ", skill, " +", bonus)
-        elif bonus == 0:
-            print("  ", skill, " ", bonus)
-        else:
-            print("  ", skill, " -", abs(bonus))
-
-    attributes_dict = CharacterInitialization.attributes_dict
     
 class CharacterGenerator():
 
@@ -87,7 +14,11 @@ class CharacterGenerator():
         print("     Human")
         attributes_dict["race"], self.attributes_dict = char_race_gen(attributes_dict)
 
-        attributes_dict["class"], self.chargen_skills_proficiency_dict, self.attributes_dict = char_class_gen(attributes_dict)
+        print("\n \n SKILL PROF BEFORE CLASS", attributes_dict["skills proficiency"])
+        
+        attributes_dict["class"], attributes_dict["skills proficiency"], self.attributes_dict = char_class_gen(attributes_dict)
+
+        print("SKILL PROF AFTER CLASS", attributes_dict["skills proficiency"])
 
         print("    ......................")
         print("\n  Now we'll choose Ability Scores.")
@@ -117,6 +48,7 @@ class CharacterGenerator():
         self.race = attributes_dict["race"]
         self.char_class = attributes_dict["class"]
         self.ability_scores = attributes_dict["ability scores"]
+        self.chargen_skills_proficiency_dict = attributes_dict["skills proficiency"]
 
 
         return {
