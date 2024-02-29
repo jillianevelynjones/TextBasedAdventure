b2d306524bab3ffcb9a998a6c5d39514b5546cf9
jillianevelynjones <126296171+jillianevelynjones@users.noreply.github.com>
2024-02-28 23:29:51 -0500

    Update CharRaceGen.py

--- TextBasedAdventure/CharRaceGen.py
+++ TextBasedAdventure/CharRaceGen.py
@@ -1,10 +1,10 @@
 def char_race_gen(attributes_dict):
-    print("\n   ......................")
-    print("\n   Now it's time to choose your race")
-    print("   Options are: ")
-    print("      Human")
+    print("\n ......................")
+    print("\n First choose a race.")
+    print("\n   Options are: ")
+    print("     Human")
     while True:
-        char_class = input(f"   Enter race: ")
+        char_class = input(f"\n   Enter race: ")
         if char_class in ("human", "Human"):
             char_race = human(attributes_dict)
             break
@@ -16,15 +16,15 @@ def char_race_gen(attributes_dict):
 
 def human (attributes_dict):
     print("\n   As a human all your ability scores increase by 1")
-    print("\n   Your size is medium")
+    print("   Your size is medium")
     attributes_dict["size"] = "5"
     print("   Your speed is 30")
     attributes_dict["speed"] = "30"
     print("   You can speak, read and write in Common and one extra language of your choice")
     attributes_dict["language 1"] = "common"
-    print("\n   Your choices are: ")
+    print("   Your choices are: ")
     print("      Elvish")
-    attributes_dict["language 2"] = input(f"   Enter language: ")
+    attributes_dict["language 2"] = input(f"\n   Enter language: ")
     if attributes_dict["language 2"] in ("elvish", "Elvish"):
         attributes_dict["language 2"] = "elvish"
     else:
