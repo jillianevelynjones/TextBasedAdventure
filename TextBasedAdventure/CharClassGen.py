ee69e7dbd84a976b617b593c86742129ee06feec
jillianevelynjones <126296171+jillianevelynjones@users.noreply.github.com>
2024-02-29 16:11:45 -0500

    Update CharClassGen.py

--- TextBasedAdventure/CharClassGen.py
+++ TextBasedAdventure/CharClassGen.py
@@ -1,5 +1,35 @@
 import CharInitialization
+import Armor
+import Weapons
+from Inventory import Inventory_Class
 
+inventory = Inventory_Class()
+
+martial_weapons = {
+    "1": Weapons.Battleaxe(),
+    "2": Weapons.Flail(),
+    "3": Weapons.Glaive(),
+    "4": Weapons.Greataxe(),
+    "5": Weapons.Greatsword(),
+    "6": Weapons.Halberd(),
+    "7": Weapons.Lance(),
+    "8": Weapons.Longsword(),
+    "9": Weapons.Maul(),
+    "10": Weapons.Morningstar(),
+    "11": Weapons.Pike(),
+    "12": Weapons.Rapier(),
+    "13": Weapons.Scimitar(),
+    "14": Weapons.Shortsword(),
+    "15": Weapons.Trident(),
+    "16": Weapons.WarPick(),
+    "17": Weapons.Warhammer(),
+    "18": Weapons.Whip(),
+    "19": Weapons.Blowgun(),
+    "20": Weapons.HandCrossbow(),
+    "21": Weapons.HeavyCrossbow(),
+    "22": Weapons.Longbow(),
+    "23": Weapons.Net()
+}
 
 def char_class_gen(attributes_dict): 
     
@@ -101,4 +131,86 @@ def fighter(class_skills_proficiency_dict, attributes_dict):
             print("Sorry! Only defense is available")
             continue
 
+    print("\n   Now it's time to choose starting equipment.")
+    choices = []
+    
+    while True:
+        print("   1. chain mail.")
+        print("   2. leather armor, longbow and 20 arrows.")
+        choice = input("   > ")
+        if choice == "1":
+            inventory.add_item(Armor.ChainMail(), 1)
+            break
+        elif choice == "2":
+            inventory.add_item(Armor.LeatherArmor(), 1)
+            inventory.add_item(Weapons.Longbow(), 1)
+            # 20 arrows
+            break
+        else:
+            print("Invalid choice. Try again.")
+            
+    while True:
+        print("   1. a martial weapon and a shield")
+        print("   2. two martial weapons")
+        choice = input("   > ")
+        if choice == "1":
+            inventory.add_item(Armor.Shield(), 1)
+            print("Martial weapons include:")
+            for key, weapon in martial_weapons.items():
+                print(f"{key}. {weapon.name}")
+            choice = input("Enter your choice: ")
+            if choice in martial_weapons:
+                inventory.add_item(martial_weapons[choice], 1)
+            else:
+                print("Invalid choice. Please choose again.")
+                continue
+            break
+                
+        elif choice == "2":
+            for i in range(2):
+                print("Choose weapon", i+1)
+                print("Martial weapons include:")
+                for key, weapon in martial_weapons.items():
+                    print(f"{key}. {weapon.name}")
+                choice = input("Enter your choice: ")
+                if choice in martial_weapons:
+                    inventory.add_item(martial_weapons[choice], 1)
+                    choices.append(choice)
+                else:
+                    print("Invalid choice. Please choose again.")
+                    continue
+                break
+        
+        else:
+            "Invalid choice. Try again."
+            continue
+
+    while True:
+        print("   1. a light crossbow and 20 bolts")
+        print("   2. two handaxes")
+        choice = input("   > ")
+        if choice == "1":
+            inventory.add_item(Weapons.LightCrossbow(), 1)
+            # 20 bolts
+            break
+        elif choice == "2":
+            inventory.add_item(Weapons.Handaxe(), 2)
+            break
+        else:
+            "Invalid choice. Try again."
+            continue
+
+    print(inventory.display_inventory())
+    '''while True:
+        print("   1. dungeoneer's pack")
+        print("   2. explorer's pack")
+        choice = input("   > ")
+        if choice == "1":
+           #code
+        elif choice == "2":
+            #code
+        else:
+            "Invalid choice. Try again."'''
+
+    
     return "fighter", class_skills_proficiency_dict, attributes_dict
