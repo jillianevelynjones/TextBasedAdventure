a1401d2c3fa36e2adac4a177b64750bad08de5a7
Jillian <126296171+jillianevelynjones@users.noreply.github.com>
2024-03-01 12:48:23 -0500

    Testing Inventory Weight

--- Testing/Test.py
+++ Testing/Test.py
@@ -1,41 +1,14 @@
-print("  \n   You're also proficient in your choice of two of the following: ")
-print("      1. Acrobatics")
-print("      2. Animal Handling")
-print("      3. Athletics")
-print("      4. History")
-print("      5. Insight")
-print("      6. Intimidation")
-print("      7. Perception")
+import Armor
+import Weapons
+import Equipment
+from Inventory import Inventory_Class
 
-chosen_skills = []
-temp_skill = ""
-while len(chosen_skills) < 2:
-    choice = input("Skill: > ")
-    if choice.isdigit():
-        choice = int(choice)
-        if 1 <= choice <= 7:
-            if choice == 1:
-                temp_skill = "acrobatics"
-            elif choice == 2:
-                temp_skill = "animal handling"
-            elif choice == 3:
-                temp_skill = "athletics"
-            elif choice == 4:
-                temp_skill = "history"
-            elif choice == 5:
-                temp_skill = "insight"
-            elif choice == 6:
-                temp_skill = "intimidation"
-            elif choice == 7:
-                temp_skill = "perception"
+inventory = Inventory_Class()
 
-            if not skills_proficiency_dict[temp_skill]:
-                skills_proficiency_dict[temp_skill] = True
-                chosen_skills.append[temp_skill]
-                print(f"You are now proficient in {temp_skill}.")
-            else:
-                print("You have already chosen proficiency in that skill.")
-        else:
-            print("Invalid choice. Please enter a number between 1 and 7.")
-    else:
-        print("Invalid input. Please enter a number.")
+print(inventory.display_inventory())
+
+print("\n adding Bedroll and Blanket")
+inventory.add_item(Equipment.Bedroll(), 1)
+inventory.add_item(Equipment.Blanket(), 1)
+
+print(inventory.display_inventory())
